a5807de02e182270816a9c69bf86f17ea60de404
rgreen88 <rynelgreen@gmail.com>
2018-01-09 13:45:21 -0500

    added dependency and example to start SQLCipher along with population

--- app/src/main/java/com/example/ryne/sqlcipher/HelloSQLCipherActivity.java
+++ app/src/main/java/com/example/ryne/sqlcipher/HelloSQLCipherActivity.java
@@ -3,14 +3,28 @@ package com.example.ryne.sqlcipher;
 import android.app.Activity;
 import android.os.Bundle;
 
+import net.sqlcipher.database.SQLiteDatabase;
+
+import java.io.File;
+
 public class HelloSQLCipherActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_sqlcipher);
+        InitializeSQLCipher();
     }
 
-
-
+    private void InitializeSQLCipher() {
+        SQLiteDatabase.loadLibs(this); //must implement using this before any database creation
+        File databaseFile = getDatabasePath("demo.db");
+        databaseFile.mkdirs();
+        databaseFile.delete();
+        SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(databaseFile,
+                "test123", null); //database creation with password parameter
+        database.execSQL("create table t1(a, b)");
+        database.execSQL("insert into t1(a, b) values(?, ?)", new Object[]{"one for the money",
+                "two for the show"});
+    }
 }
