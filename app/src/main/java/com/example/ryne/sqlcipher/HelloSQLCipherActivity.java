1655532aa8fdb04bba2990eef92d8f2438fc4782
rgreen88 <rynelgreen@gmail.com>
2018-01-09 13:21:05 -0500

    first commit

---
+++ app/src/main/java/com/example/ryne/sqlcipher/HelloSQLCipherActivity.java
@@ -0,0 +1,16 @@
+package com.example.ryne.sqlcipher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloSQLCipherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_sqlcipher);
+    }
+
+
+
+}
